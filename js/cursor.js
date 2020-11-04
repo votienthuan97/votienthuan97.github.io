a4ae6a46e298123a3e945495e18047645849c871
unknown <votienthuan97@gmail.com>
2020-11-04 22:10:37 +0700

    add btn music

--- js/cursor.js
+++ js/cursor.js
@@ -22,6 +22,21 @@ $(document).ready(function () {
         $('.point__hand').css("opacity", "0");
         $('.oval__textHand').css("opacity", "0");
     });
+    $(".leftContent__sound").hover(function() {
+        $('.point__default').css("display", "none");
+        $('.point__default').css("opacity", "0");
+        $('.point__music').css("display", "block");
+        $('.point__music').css("opacity", "1");
+        $('.oval__textMusic').css("opacity", "1");
+    }, function() {
+        $('.point__default').css("display", "block");
+        $('.point__default').css("opacity", "1");
+        $('.point__music').css("display", "none");
+        $('.point__music').css("opacity", "0");
+        $('.oval__textMusic').css("opacity", "0");
+    });
+
+    
 });
 // SET POSITION CURSOR
 function setPosition(element, e) {
--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
-    
+    // PLAY PAUSE MUSIC BACKGROUND
+    const btnPlayPause = document.getElementById("bgMusic");
+    $("#soundPlayPause").click(function(){
+        if($(this).prop("checked") == true){
+            $(".icon__item").css("background-color", "#F2DA87");
+            btnPlayPause.play();
+        }
+        else if($(this).prop("checked") == false){
+            $(".icon__item").css("background-color", "#CCC");
+            btnPlayPause.pause();
+        }
+    });
 });
 
 
