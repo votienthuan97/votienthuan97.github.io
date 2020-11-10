0493386e830390331ac7ef0a191abbfdeae9c0cb
unknown <votienthuan97@gmail.com>
2020-11-10 19:54:16 +0700

    fix fix fgi

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+    // PLAY VIDEO MOBILE
+    const videoContactLoopMobile = document.getElementById("vdContactLoopMobile");
+    setTimeout(function() {
+        videoContactLoopMobile.play();
+    }, 400);
     // PLAY PAUSE MUSIC BACKGROUND
     const btnPlayPause = document.getElementById("bgMusic");
     $("#soundPlayPause").click(function(){
