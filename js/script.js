54fd545b4f396f91973a64de11eac43694141026
unknown <votienthuan97@gmail.com>
2020-11-10 11:41:13 +0700

    update code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    document.body.addEventListener('touchmove', function(e){ 
+        document.getElementsByTagName('body')[0]. style .height = "100vh";
+        document.getElementsByTagName('body')[0]. style. overflow = "hidden";
+      });
     // PLAY PAUSE MUSIC BACKGROUND
     const btnPlayPause = document.getElementById("bgMusic");
     $("#soundPlayPause").click(function(){
