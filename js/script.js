cb1e64dd294b986d031edb67c61d88a7c494aa68
unknown <votienthuan97@gmail.com>
2020-11-10 12:08:02 +0700

    update code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+    const appHeight = () => {
+        const doc = document.documentElement
+        doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+    }
+    window.addEventListener('resize', appHeight)
+    appHeight();
     // PLAY PAUSE MUSIC BACKGROUND
     const btnPlayPause = document.getElementById("bgMusic");
     $("#soundPlayPause").click(function(){
