1a91d864b00f8ebebf8883c89b0ad1f5028ae8e7
unknown <votienthuan97@gmail.com>
2020-11-10 19:51:26 +0700

    push lan cuoi met qua

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     // PLAY VIDEO MOBILE
     const videoContactLoopMobile = document.getElementById("vdContactLoopMobile");
-    videoContactLoopMobile.load();
-    videoContactLoopMobile.play();
+    setTimeout(function() {
+        videoContactLoopMobile.load();
+        videoContactLoopMobile.play();
+    }, 400);
     // PLAY PAUSE MUSIC BACKGROUND
     const btnPlayPause = document.getElementById("bgMusic");
     $("#soundPlayPause").click(function(){
